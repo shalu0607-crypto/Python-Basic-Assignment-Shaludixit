0da2291bd784bdd6c9961399b5a176fefc9c13cb
shalu0607-crypto <shaludixit2525@gmail.com>
2026-09-07 19:41:52 +0530

    Add student details input and display functionality

---
+++ 02_student_details.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+roll_number = input("Enter your roll number: ")
+course = input("Enter your course: ")
+semester = input("Enter your semester: ")
+
+print("Name:", name)
+print("Roll Number:", roll_number)
+print("Course:", course)
+print("Semester:", semester)
